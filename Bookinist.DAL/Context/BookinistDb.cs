2a8e370a3ad22adfb847316d7c979d186155e1cd
AlexanderDix <axdxrere@gmail.com>
2022-04-12 15:10:06 +0500

    Структура БД

---
+++ Bookinist.DAL/Context/BookinistDb.cs
@@ -0,0 +1,17 @@
+﻿using Bookinist.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookinist.DAL.Context;
+
+public class BookinistDb : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Buyer> Buyers { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Seller> Sellers { get; set; }
+    public DbSet<Deal> Deals { get; set; }
+
+    public BookinistDb(DbContextOptions<BookinistDb> options) : base(options)
+    {
+    }
+}
